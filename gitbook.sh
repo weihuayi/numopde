b43c2c65adefa753a54b786ff730c28216170d58
Huayi Wei <weihuayi@xtu.edu.cn>
2019-04-12 09:09:25 +0800

    update

--- gitbook.sh
+++ gitbook.sh
@@ -1,14 +1,16 @@
-cd ../weihuayi.github.io/
-git pull
-
-cd ../numopde/
-git add .
-git commit -m "update" 
-git pull
-git push 
-gitbook build
-cp -r _book/* ../weihuayi.github.io/math/numopde/
-cd ../weihuayi.github.io/
-git add .
-git commit -m "update" 
-git push 
+CHANGED=$(git diff-index --name-only HEAD --)
+if [ ! -z $CHANGED ];then
+    cd ../weihuayi.github.io/
+    git pull
+    cd ../numopde/
+    git add .
+    git commit -m "update" 
+    git pull
+    git push 
+    gitbook build
+    cp -r _book/* ../weihuayi.github.io/math/numopde/
+    cd ../weihuayi.github.io/
+    git add .
+    git commit -m "update" 
+    git push 
+fi
