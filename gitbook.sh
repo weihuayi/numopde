c96631f40ef0a7b09c701cb8eafd1be29673e4d0
Huayi Wei <weihuayi@xtu.edu.cn>
2021-03-26 09:38:44 +0800

    update

--- gitbook.sh
+++ gitbook.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 if [[ `git status --porcelain` ]]; then
     cd ../weihuayi.github.io/
-    git pull
+    git pull origin
     cd ../numopde/
     git add .
     git commit -m "update" 
-    git pull
-    git push 
+    git pull origin
+    git push origin 
     gitbook build
     cp -r _book/* ../weihuayi.github.io/math/numopde/
     cd ../weihuayi.github.io/
     git add .
     git commit -m "update" 
-    git push 
+    git push origin 
 else
-    git pull
-    git push
+    git pull origin
+    git push origin
 fi
